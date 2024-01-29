3423c41bd27217baf374fa20bbeedb293b0ce0e0
gnzat <tazngmt@gmail.com>
2024-01-29 23:20:52 +0800

    components created

---
+++ src/app/add-entry/add-entry.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-entry',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-entry.component.html',
+  styleUrl: './add-entry.component.css'
+})
+export class AddEntryComponent {
+
+}
